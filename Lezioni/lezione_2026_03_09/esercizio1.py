392b5cd9c4af0b2e7b42fee571107f4c9b01fb99
Daniele Cerrina <daniecerri@gmail.com>
2026-03-09 20:22:57 +0100

    Flowchart lezione 09/03/2026

---
+++ Lezioni/lezione_2026_03_09/esercizio1.py
@@ -0,0 +1,11 @@
+lato = 45.6  # Abbiamo creato una variabile che si chiama "lato" e ci abbiamo messo 5
+area = lato ** 2  # Abbiamo creato una variabile
+# "area" e ci abbiamo messo il risultato del lato * lato
+print(f"L'area del quadrato è {area}")
+print(f"L'area del quadrato di lato {lato} è {area}")
+
+
+
+
+
+
