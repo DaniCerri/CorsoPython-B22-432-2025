ee4ec0943be20d9275d16922d9d43f88e0a3d357
Daniele Cerrina <daniecerri@gmail.com>
2026-03-13 15:23:43 +0100

    Inizio nuovo esercizio

---
+++ Lezioni/lezione_2026_03_13/esercizio1.py
@@ -0,0 +1,9 @@
+lato = 45.6  # TODO: prendere in input il lato e convertirlo in numero con la virgola
+area = lato ** 2  # calcoliamo l'area
+print(f"L'area del quadrato di lato {lato} è {area}")  # Stampiamo il risultato
+
+
+
+
+
+
