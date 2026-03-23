da103eb32c9cbd1df1f646cd38d8a997d66d0b2a
Daniele Cerrina <daniecerri@gmail.com>
2026-03-23 18:24:05 +0100

    Checkpoint lezione 23/03/2026

--- Lezioni/lezione_2026_03_23/esercizio2_for.py
+++ Lezioni/lezione_2026_03_23/esercizio2_for.py
@@ -2,6 +2,12 @@
 N_OGGETTI = 5
 
 # definiamo la lista
-...
+lista_oggetti = []
+
+for i in range(N_OGGETTI):  # Per i che va da 0 a N_OGGETTI - 1 (N_OGGETTI volte)
+    oggetto = input(f"Inserisci l'oggetto numero {i + 1} da comprare: ")  # Prendiamo in input il nome di un oggetto
+    lista_oggetti.append(oggetto)  # Lo aggiungiamo alla lista
+
+print(lista_oggetti)
 
 
