255ca555bf5cbaaeddc8191391d35cec82a86794
Daniele Cerrina <daniecerri@gmail.com>
2026-03-23 20:34:56 +0100

    Lezione 23/03/2026

--- Lezioni/lezione_2026_03_23/esercizio2_for.py
+++ Lezioni/lezione_2026_03_23/esercizio2_for.py
@@ -10,4 +10,30 @@ for i in range(N_OGGETTI):  # Per i che va da 0 a N_OGGETTI - 1 (N_OGGETTI volte
 
 print(lista_oggetti)
 
+# Adesso per ogni oggetto chiediamo all'utente (in maniera chiara) quanto costa l'oggetto
+# Salviamo tutti i prezzi in una lista di prezzi (i prezzi in questa lista devono lo stesso indice dell'oggetto a cui fanno
+# riferimento)
+# Controllare con un while che il prezzo sia > 0
+lista_prezzi = []
+for oggetto in lista_oggetti:
+    while True:
+        prezzo_oggetto = float(input(f"Inserisci il prezzo dell'oggeto '{oggetto}': "))
+        if prezzo_oggetto > 0:
+            break
+
+        print("Il prezzo deve essere maggiore di zero")
+
+    lista_prezzi.append(prezzo_oggetto)
+
+print(lista_prezzi)
+
+# Stampare il totale della spesa comprando tutti gli oggetti (somma di lista_prezzi)
+totale = 0
+for prezzo in lista_prezzi:
+    totale += prezzo
+
+print(f"La spesa totale è {totale:.2f} €")
+
+
+
 
