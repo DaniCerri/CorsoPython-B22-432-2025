396f52576250275d1b74bb5f52956f18771d6d19
Daniele Cerrina <daniecerri@gmail.com>
2026-03-30 20:16:06 +0200

    Lezione 30/03/2026

--- Lezioni/lezione_2026_03_30/esercizio2.py
+++ Lezioni/lezione_2026_03_30/esercizio2.py
@@ -8,3 +8,32 @@ Ne calcoliamo la media
 * La somma di una lista di interi si trova con sum(lista_interi)
 * La lunghezza di una lista è len(lista)
 """
+
+while True:
+    # TODO per il futuro: controllare anche che siano tutti numeri che si convertano ad interi
+    numeri = input("Inserisci i tuoi numeri interi separati da spazi: ")
+    if not numeri: # Se numeri è una stringa vuota
+        print("Inserisci almeno un numero")
+        continue
+
+    # Se l'utente ha inserito qualcosa, assumiamo che vada bene e interrompiamo il while
+    break
+
+# 1. Splittiamo la stringa in base agli spazi
+lista_numeri_str = numeri.split(" ")
+
+# 2. Per ogni elemento della lista di numeri in formato stringa, lo convertiamo a intero
+lista_numeri_int = []
+for numero_str in lista_numeri_str:
+    numero_int = int(numero_str)  # Convertiamo la stringa a intero
+    lista_numeri_int.append(numero_int)  # Aggiungiamo il numero convertito alla lista
+
+# lista_numeri_int = [int(num) for num in numeri.split(" ")]  # Per approfondimento -> list comprehension
+# il codice sopra fa esattamente i punti 1 e 2
+
+# 3. Calcoliamo la media
+media = sum(lista_numeri_int) / len(lista_numeri_int)
+
+# 4. Stampiamo la media
+print(f"La media della lista {lista_numeri_int} è {media}")
+
