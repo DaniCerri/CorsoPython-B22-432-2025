202d4693db0298072c339a6239a76c714a9dca46
Daniele Cerrina <daniecerri@gmail.com>
2026-04-10 20:23:47 +0200

    Fine lezione 10/04/2026

--- Lezioni/lezione_2026_04_10/funzioni3.py
+++ Lezioni/lezione_2026_04_10/funzioni3.py
@@ -11,15 +11,39 @@ def calcoli(numero: int):
     lista = [numero ** 2, numero ** 3, numero ** 0.5]
     return lista
 
+# Esercizio 3: fare una funzione che prenda due numeri interi e dica True se esiste un numero che li
+# divida entrambi, False altrimenti
+# a è divisibile per b se a % b == 0
+def not_coprimi(numero1: int, numero2: int):
+    riferimento = min(numero1, numero2)  # Prendiamo il minore come riferimento
+    if numero1 % 2 == 0 and numero2 % 2 == 0:  # Controlliamo subito se sono entrambi pari
+        return True
+
+    print(int(riferimento ** 0.5) + 1)
+    # Per ogni numero i da 3 alla radice del riferimento + 1, a salti di due -> [3, 5, 7, ...]
+
+    # TODO: trovare il miglior numero centrale per fare meno controlli possibile
+    for i in range(3, int(riferimento // 2) + 1, 2):
+    # for i in range(int(riferimento // 2) + 1, 3, -2):  # [..., 7, 5, 3] Per fare questo dovremmo sapere se si parte da un numero pari o dispari
+        print(i)
+        if numero1 % i == 0 and numero2 % i == 0:
+            return True
+
+    return False
+
+
+
+
 if __name__ == "__main__":
     # lista_di_stringhe = ["ciao", "come", "va"]
     # print(lista_di_stringhe)
     # lista_maiusc = lista_maiuscola(lista_di_stringhe)
     # print(lista_maiusc)
-    numero_int = 10
-    result = calcoli(numero_int)
-    print(result)
-
+    # numero_int = 10
+    # result = calcoli(numero_int)
+    # print(result)
+    num1, num2 = 10, 15
+    print(not_coprimi(num1, num2))
 
 
 
---
+++ Lezioni/lezione_2026_04_10/funzioni_es.py
@@ -0,0 +1,14 @@
+"""
+Facciamo una funzione per calcolare la media di una lista NON ORDINATA
+Usiamo la stessa funzione per calcolare la deviazione standard:
+    1. Calcolo della media
+    2. Si fa una lista con le differenze tra gli elementi della lista originale e la media
+    3. Ogni elemento di questa lista si eleva al quadrato
+    4. Si fa la media (sempre con la funzione di prima) di questa nuova lista
+    5. Si fa la radice quadrata di questa media
+Facciamo una funzione che data una lista ne calcoli media e deviazione standard e le stampi in questo modo:
+Lista: [...], Media: media:.2f, Std: std:.2f
+
+# --------------------- BONUS ---------------------
+Calcoliamo anche la massima differenza tra i membri della lista e la mettiamo nella stampa di sopra
+"""
