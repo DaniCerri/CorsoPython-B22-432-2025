e30ad2c9559a4b4c6f341ac07a2fe6212df40dac
Daniele Cerrina <daniecerri@gmail.com>
2026-04-24 18:08:57 +0200

    Checkpoint lezione 24/04

--- Lezioni/lezione_2026_04_24/ripasso2.py
+++ Lezioni/lezione_2026_04_24/ripasso2.py
@@ -36,6 +36,46 @@ def codifica(stringa: str):
 
     return stringa_kebab, stringa_camel
 
+"""
+Vogliamo fare una funzione che calcoli quanti numeri sono sopra e sotto una soglia data
+all'interno di una lista
+Soglia -> Threshold
+Es: lista = [1, 2, 3, 4, 5, 6, 7], soglia = 3.4  ----> Minori: 3, Maggiori: 4
+"""
+def conta_soglia(lista: list[float | int], soglia: float):  # Errore bonus: va bene sia float che int
+    minori = 0  # Errore 1.1: inizializziamo i contatori a 0
+    maggiori = 0  # Errore 1.2: inizializziamo i contatori a 0
+    for n in lista:  # Errore 2: dobbiamo prendere proprio gli elementi, non indici
+        if n < soglia:
+            minori += 1  # Errore 3: Dobbiamo aumentare i contatori e non resettarli
+        elif n > soglia:  # Errore 4: dobbiamo gestire il caso in cui si è uguali alla soglia
+            maggiori += 1
+
+    return minori, maggiori  # Errore 5: Il return era fuori dalla funzione
+
+"""
+Facciamo una funzione che date due liste restituisca una matrice (lista di liste)
+con tutte le possibili coppie (prodotto cartesiano)
+
+[1, 2, 3] x [4, 5, 6] -> 
+[
+    (1, 4), (1, 5), (1, 6),
+    (2, 4), (2, 5), (2, 6),
+    (3, 4), (3, 5), (3, 6)
+]
+"""
+def prodotto_cartesiano(lista1: list, lista2: list):
+    prodotto = ()
+    for elem1 in lista1:
+        for elem2 in lista2:
+            prodotto += (elem1, elem2)
+
+    for elem2 in lista2:
+        for elem1 in lista1:
+            prodotto += (elem2, elem1)
+
+    return lista1, lista2
+
 
 
 
